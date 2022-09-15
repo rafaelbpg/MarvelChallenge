c655c4eccfc0c7ee6c2c3058e53b216fa79a7969
Rafael Pérez <rafaelbenitoperezgonzalez@gmail.com>
2022-09-15 07:24:33 +0200

    Hero list and proxy

---
+++ providers/ProxyProviderContext.tsx
@@ -0,0 +1,454 @@
+import {ApisauceInstance, create} from 'apisauce';
+import * as React from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import {Button, FlatList, Image, Pressable, Text, View} from 'react-native';
+import tw from 'twrnc';
+
+type MarvelHeroesListResponse = {
+  code: number;
+  status: string;
+  copyright: string;
+  attributionText: string;
+  attributionHTML: string;
+  data: MarvelHeroData;
+  etag: string;
+};
+
+type MarvelHeroComicsListResponse = {
+  code: number;
+  status: string;
+  copyright: string;
+  attributionText: string;
+  attributionHTML: string;
+  data: MarvelComicData;
+  etag: string;
+};
+
+type MarvelResponse = MarvelHeroesListResponse | MarvelHeroComicsListResponse;
+type HeroType = {
+  id: number;
+  name: string;
+  description: string;
+  modified: Date;
+  resourceURI: string;
+  urls: Array<{
+    type: string;
+    url: string;
+  }>;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  comics: MarvelComicData;
+  stories: {
+    available: number;
+    returned: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+      type: string;
+    }>;
+  };
+  events: {
+    available: number;
+    returned: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+    }>;
+  };
+  series: {
+    available: number;
+    returned: number;
+    collectionURI: string;
+    items: Array<{
+      resourceURI: string;
+      name: string;
+    }>;
+  };
+};
+type MarvelHeroData = Array<{
+  offset: number;
+  limit: number;
+  total: number;
+  count: number;
+  results: Array<HeroType>;
+}>;
+
+type MarvelComicData = Array<{
+  offset: number;
+  limit: number;
+  total: number;
+  count: number;
+  results: Array<{
+    id: number;
+    digitalId: number;
+    title: string;
+    issueNumber: number;
+    variantDescription: string;
+    description: string;
+    modified: Date;
+    isbn: string;
+    upc: string;
+    diamondCode: string;
+    ean: string;
+    issn: string;
+    format: string;
+    pageCount: number;
+    textObjects: Array<{
+      type: string;
+      language: string;
+      text: string;
+    }>;
+    resourceURI: string;
+    urls: Array<{type: string; url: string}>;
+    series: {
+      resourceURI: string;
+      name: string;
+    };
+    variants: Array<{
+      resourceURI: string;
+      name: string;
+    }>;
+    collections: Array<{resourceURI: string; name: string}>;
+    collectedIssues: Array<{resourceURI: string; name: string}>;
+    dates: Array<{type: string; date: Date}>;
+    prices: Array<{type: string; price: number}>;
+    thumbnail: {
+      path: string;
+      extension: string;
+    };
+    images: Array<{path: string; extension: string}>;
+    creators: {
+      available: number;
+      returned: number;
+      collectionURI: string;
+      items: Array<{
+        resourceURI: string;
+        name: string;
+        role: string;
+      }>;
+    };
+    characters: {
+      available: number;
+      returned: number;
+      collectionURI: string;
+      items: Array<{
+        resourceURI: string;
+        name: string;
+        role: string;
+      }>;
+    };
+    stories: {
+      available: number;
+      returned: number;
+      collectionURI: string;
+      items: Array<{
+        resourceURI: string;
+        name: string;
+        type: string;
+      }>;
+    };
+    events: {
+      available: number;
+      returned: number;
+      collectionURI: string;
+      items: Array<{
+        resourceURI: string;
+        name: string;
+      }>;
+    };
+  }>;
+}>;
+type MarvelData = MarvelHeroData | MarvelComicData;
+
+type ContextStateUninitialized = {
+  url?: undefined;
+  isFetching: false;
+  data?: undefined;
+};
+
+type ContextStateInitialized = {
+  url: string;
+  isFetching: false;
+  data?: undefined;
+};
+
+type ContextStateFetching<T> = {
+  url: string;
+  isFetching: true;
+  data?: T;
+};
+
+type ContextStateFetched<T> = {
+  url: string;
+  isFetching: false;
+  data: T;
+  apisauceInstance: ApisauceInstance;
+};
+
+type ApiRequestContextState<T> =
+  | ContextStateUninitialized
+  | ContextStateInitialized
+  | ContextStateFetching<T>
+  | ContextStateFetched<T>;
+
+interface IActions {
+  paginate(): void;
+}
+
+const initialState = {
+  isFetching: false,
+};
+
+type Props = {
+  url: string;
+  maxResultsPerPage: number;
+  children: JSX.Element;
+};
+
+type ProxyHandler<T, P extends string> = {
+  get?(target: T, p: P, receiver: any): any;
+  set?(
+    target: {results: {[key in P]?: T}},
+    p: P,
+    value: any,
+    receiver: any,
+  ): boolean;
+};
+
+declare const Proxy: {
+  new <T extends object>(
+    target: {results: {[key in string]?: T}; apiInstance: ApisauceInstance},
+    handler: ProxyHandler<T, string>,
+  ): {[key: string]: Promise<T>};
+};
+
+const marvelProxy = new Proxy<MarvelResponse>(
+  {
+    apiInstance: create({baseURL: 'https://gateway.marvel.com:443/v1/public/'}),
+    results: {},
+  },
+  {
+    get: function <T extends MarvelResponse>(
+      target: {
+        results: {
+          [key in string]?: T;
+        };
+      },
+      url: string,
+    ) {
+      const values = target;
+      return new Promise<T>(async (resolve, reject) => {
+        if (values.results.hasOwnProperty(url)) {
+          resolve(values.results[url] as T);
+          return;
+        }
+        try {
+          const response = await (
+            target as {
+              results: {
+                [key in string]?: T;
+              };
+              apiInstance: ApisauceInstance;
+            }
+          ).apiInstance.get<T>(url);
+          const {data} = response;
+          if (data?.status !== 'Ok') {
+            throw new Error('Error fetching data');
+          }
+          (
+            target as {
+              results: {
+                [key in string]?: T;
+              };
+            }
+          ).results[url] = data;
+          resolve(data);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    },
+    set: (target, url: string, value) => {
+      target.results[url] = value;
+      return true;
+    },
+  },
+);
+
+const ApiRequestContext = createContext<
+  [ApiRequestContextState<MarvelData>, IActions]
+>([initialState as ContextStateUninitialized, {paginate: () => undefined}]);
+type AuthParamsType = {
+  apikey: string;
+  ts: string;
+  hash: string;
+};
+type QueryStringParamsType = {
+  limit: string;
+  offset: string;
+};
+
+function getAuthQueryStringParams(): AuthParamsType {
+  const apikey = 'e6470df55c0bd79b8f0a8d4bddc73a62';
+  const ts: string = new Date().getTime().toString();
+  const privateKey = '9bc994ff81c9693f0e3c03a7e29fd34abef6e5ec';
+  const md5 = require('md5');
+  const hash = md5(`${ts}${privateKey}${apikey}`);
+
+  return {
+    apikey,
+    ts,
+    hash,
+  };
+}
+
+function getPaginationQueryStringParams(
+  maxResults: number,
+  page: number,
+): QueryStringParamsType {
+  const limit = maxResults.toString();
+  const offset = (page * maxResults).toString();
+  return {
+    limit,
+    offset,
+  };
+}
+
+export function CachedRequestsProvider({
+  children,
+  url,
+  maxResultsPerPage,
+}: Props) {
+  const [state, setState] = useState<ApiRequestContextState<MarvelData>>({
+    isFetching: false,
+    url,
+  } as ContextStateInitialized);
+
+  const [page, setPage] = useState(0);
+  const paginate = (): void => {
+    setPage(page + 1);
+  };
+  const getNavigatableUrl = useCallback((): string => {
+    const newUrl = new URL(url);
+    Object.entries({
+      ...getAuthQueryStringParams(),
+      ...getPaginationQueryStringParams(maxResultsPerPage, page),
+    }).forEach((param) => {
+      newUrl.searchParams.append(param[0], param[1]);
+    });
+    return newUrl.toString().replace('/?', '?') + '/';
+  }, [page, state]);
+
+  useEffect(() => {
+    console.log('Fetching');
+    if (state.isFetching || !state.url) {
+      return;
+    }
+    setState(
+      state.url !== url
+        ? {
+            isFetching: true,
+            url,
+          }
+        : {
+            ...state,
+            isFetching: true,
+          },
+    );
+    marvelProxy[getNavigatableUrl()].then((value) => {
+      const newResults = value?.data?.results
+      const oldResults = state?.data?.characters?.data?.results
+      if (newResults && oldResults){
+        const mixedResults = oldResults.concat(newResults)
+        value.data.results = mixedResults
+      }
+      setState({
+        ...state,
+        isFetching: false,
+        data: {
+          ...(state.data ?? {}),
+          [url]: value,
+        },
+      } as ContextStateFetched<MarvelData>);
+    });
+  }, [page, url]);
+
+  return (
+    <ApiRequestContext.Provider value={[state, {paginate}]}>
+      {children}
+    </ApiRequestContext.Provider>
+  );
+}
+
+export const useCachedRequests = (): [
+  ApiRequestContextState<MarvelData>,
+  IActions,
+] => {
+  return useContext(ApiRequestContext);
+};
+const Hero: React.FC<HeroType> = (props) => {
+  const urlImage =
+    `${props.thumbnail.path}.${props.thumbnail.extension}`.replace(
+      'http://',
+      'https://',
+    );
+  return (
+    <View>
+      <Pressable
+        onPress={() => 1}
+        style={{
+          alignSelf: 'center',
+          marginTop: 20,
+          borderStyle: 'solid',
+          borderColor: 'white',
+          borderWidth: 2,
+          padding: 12,
+        }}>
+        <Text style={tw`text-white text-center text-xl mb-1`}>
+          {props.name}
+        </Text>
+        <Image source={{uri: urlImage}} style={{width: 300, height: 300}} />
+        <Text style={tw`text-lg text-white text-center mt-1`}>{props.comics.available} comics</Text>
+      </Pressable>
+    </View>
+  );
+};
+function HeroesList() {
+  const [state, actions] = useCachedRequests();
+  const [heroes, setHeroes] = useState<MarvelData>([]);
+  useEffect(() => {
+    setHeroes(state?.data?.characters?.data.results);
+  }, [state, setHeroes]);
+
+  return (
+    <View>
+      <Text style={tw`text-white text-4xl text-center mt-5`}>Hero list</Text>
+      <FlatList
+        data={heroes}
+        keyExtractor={(heroe) => heroe.id.toString()}
+        renderItem={(heroe: HeroType, index) => <Hero {...heroe.item} />}
+        onEndReached={actions.paginate}
+      />
+      <Button title={'Paginate'} onPress={() => actions.paginate()} />
+    </View>
+  );
+}
+
+function ExampleProvidedComponent({url}: {url: string}) {
+  return (
+    <CachedRequestsProvider maxResultsPerPage={10} url={url}>
+      <HeroesList />
+    </CachedRequestsProvider>
+  );
+}
+export {ApiRequestContext, ExampleProvidedComponent};
--- screens/LoginScreen/index.tsx
+++ screens/LoginScreen/index.tsx
@@ -1,57 +1,78 @@
-import React from 'react'
-import { Text, View, Image, TextInput, Pressable } from 'react-native'
-import tw from 'twrnc'
-import LoginScreenController from './LoginScreenController'
-
-const LoginScreen : React.FC = () => {
-    const {username, password, setUsername, setPassword, fillWithTestUser, signIn} = LoginScreenController()
-    return( 
-        <View style={tw`bg-black h-full`}>
-            <View style={tw`flex flex-col items-center justify-center py-32 w-full px-4 mx-auto `}>
-                <View>
-                    <Image
-                        style={{width:200, height:80}}
-                        source={require('../../marvellogo.png')} />
-                </View>
-                <View style={tw`w-full rounded-lg shadow bg-white mt-4 py-8`}>
-                    <View>
-                        <Text style={tw`text-2xl text-center font-bold leading-tight tracking-tight text-gray-900`}>
-                            Sign in to your account
-                        </Text>
-                    </View>
-                    <View style={tw`mt-4 px-2`}>
-                        <TextInput
-                            value={username}
-                            style={tw`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5`}
-                            onChangeText = { text => setUsername(text)}
-                            placeholder='Username...' />
-                    </View>
-                    <View style={tw`mt-4 px-2`}>
-                        <TextInput
-                            value={password}
-                            secureTextEntry={true}
-                            onChangeText = { text => setPassword(text)}
-                            style={tw`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5`}
-                            placeholder='Password' />
-                    </View>
-                    <View style={tw`mt-4 px-2`}>
-                        <Pressable
-                            onPressIn={(event) => signIn()}
-                            style={({ pressed }) => [ tw`w-full p-4 rounded-xl pressed:bg-blue-500 ${pressed ? 'bg-red-500' : 'bg-red-600'}`]}>
-                                <Text style={tw`text-white text-center text-5`}>Sign in</Text>
-                        </Pressable>
-                    </View>
-                    <View style={tw`mt-4 px-2`}>
-                        <Pressable
-                            onPressIn={(event) => fillWithTestUser()}
-                            style={({ pressed }) => [
-                                tw`w-full bg-black p-4 rounded-xl ${pressed ? 'bg-gray-900' : ''}`]}>
-                                <Text style={tw`text-white text-center text-5`}>Fill with test user</Text>
-                        </Pressable>
-                    </View>
-                </View>
-            </View>
+import React, {useState} from 'react';
+import {Text, View, Image, TextInput, Pressable} from 'react-native';
+import tw from 'twrnc';
+import LoginScreenController from './LoginScreenController';
+import {ApisauceInstance, create} from 'apisauce';
+const LoginScreen: React.FC = () => {
+  const {
+    username,
+    password,
+    setUsername,
+    setPassword,
+    fillWithTestUser,
+    signIn,
+  } = LoginScreenController();
+  return (
+    <View style={tw`bg-black h-full`}>
+      <View
+        style={tw`flex flex-col items-center justify-center py-32 w-full px-4 mx-auto `}>
+        <View>
+          <Image
+            style={{width: 200, height: 80}}
+            source={require('../../marvellogo.png')}
+          />
         </View>
-    )
+        <View style={tw`w-full rounded-lg shadow bg-white mt-4 py-8`}>
+          <View>
+            <Text
+              style={tw`text-2xl text-center font-bold leading-tight tracking-tight text-gray-900`}>
+              Sign in to your account
+            </Text>
+          </View>
+          <View style={tw`mt-4 px-2`}>
+            <TextInput
+              value={username}
+              style={tw`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5`}
+              onChangeText={(text) => setUsername(text)}
+              placeholder="Username..."
+            />
+          </View>
+          <View style={tw`mt-4 px-2`}>
+            <TextInput
+              value={password}
+              secureTextEntry={true}
+              onChangeText={(text) => setPassword(text)}
+              style={tw`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg p-2.5`}
+              placeholder="Password"
+            />
+          </View>
+          <View style={tw`mt-4 px-2`}>
+            <Pressable
+              onPressIn={(event) => signIn()}
+              style={({pressed}) => [
+                tw`w-full p-4 rounded-xl pressed:bg-blue-500 ${
+                  pressed ? 'bg-red-500' : 'bg-red-600'
+                }`,
+              ]}>
+              <Text style={tw`text-white text-center text-5`}>Sign in</Text>
+            </Pressable>
+          </View>
+          <View style={tw`mt-4 px-2`}>
+            <Pressable
+              onPressIn={(event) => fillWithTestUser()}
+              style={({pressed}) => [
+                tw`w-full bg-black p-4 rounded-xl ${
+                  pressed ? 'bg-gray-900' : ''
+                }`,
+              ]}>
+              <Text style={tw`text-white text-center text-5`}>
+                Fill with test user
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 export default LoginScreen;
